691b687c9906e2322fd71b1b3f0bae1d4ba9e934
Jeevesh8 <creativityinczenyoga@gmail.com>
2022-05-06 22:54:27 +0530

    try tf 1.5 version, as that available on pip

--- prepare.sh
+++ prepare.sh
@@ -10,6 +10,6 @@ source /ext3/env.sh
 pip install getgist
 getgist raffaem download_glue_data.py
 conda install python=3.7
-pip install -Iv tensorflow-gpu==1.11.0
+pip install -Iv tensorflow-gpu==1.15.0
 python3 download_glue_data.py --data_dir glue_data --tasks QQP
 "
