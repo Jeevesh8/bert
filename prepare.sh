3ee8a4a797c72ee09d3c9cb7bc640db1db8dac6d
Jeevesh8 <creativityinczenyoga@gmail.com>
2022-05-07 10:12:24 +0530

    Update package installs and time requirement * Reduce time requirement * Install TF with conda to make sure auto downgrade of other packages(on which tf depends) also happens * Downgrade version of numpy for propoer working with TF=1.15

--- prepare.sh
+++ prepare.sh
@@ -10,6 +10,8 @@ source /ext3/env.sh
 pip install getgist
 getgist raffaem download_glue_data.py
 conda install python=3.7
-pip install -Iv tensorflow-gpu==1.15.0
+conda install tensorflow-gpu==1.15.0
+pip install numpy==1.19.5
 python3 download_glue_data.py --data_dir glue_data --tasks QQP
+python3 \"import tensorflow as tf; print(tf.__version__);\"
 "
